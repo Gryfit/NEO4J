aae85a0e401fd31071054703269caaa61aa293a1
Gryfit <bartektonia@gmail.com>
2019-01-06 14:54:18 +0100

    Solution

--- src/main/scala/Solution.scala
+++ src/main/scala/Solution.scala
@@ -14,14 +14,24 @@ class Solution() {
     println(findMovieRecommendationForUser("emileifrem"))
   }
 
-  private def findActorByName(actorName: String) = ???
+  private def findActorByName(actorName: String) =
+    graphDatabase.runCypher("MATCH (p:Actor) WHERE p.name = \'" + actorName + "\' RETURN p")
 
-  private def findMovieByTitleLike(movieName: String) = ???
+  private def findMovieByTitleLike(movieName: String) =
+    graphDatabase.runCypher("MATCH (m:Movie) where m.title CONTAINS \'" + movieName + " \' RETURN m")
 
-  private def findRatedMoviesForUser(userLogin: String) = ???
+  private def findRatedMoviesForUser(userLogin: String) =
+    graphDatabase.runCypher("MATCH (u: User {login : \'" + userLogin + "\'})-[:RATED]->(m: Movie) RETURN m")
 
-  private def findCommonMoviesForActors(actorOne: String, actrorTwo: String) = ???
+  private def findCommonMoviesForActors(actorOne: String, actrorTwo: String) =
+    graphDatabase.runCypher(
+      "MATCH (f:Actor {name: \'" + actorOne + "\'})-[:ACTS_IN]->" +
+        "(m)<-[:ACTS_IN]-(s: Actor {name: \'" + actrorTwo + "\'}) RETURN m"
+    )
 
-  private def findMovieRecommendationForUser(userLogin: String) = ???
+
+  private def findMovieRecommendationForUser(userLogin: String) =
+    graphDatabase.runCypher("Match (user: Person{login: \'" + userLogin + "\'})-[:RATED]->" +
+      "(m: Movie)<-[:RATED]-(user2: User)-[:RATED]->(movies: Movie) return movies.title ")
 
 }
